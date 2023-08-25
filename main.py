89e9d357d912ba21d1b1c9e175bf5b72e926186e
timmashkov <bodomizer#yandex.ru>
2023-08-25 13:21:39 +0300

    added some changes

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Hello world'
+    return f'''<p1>Hello world</p1>
+<p2>This is the initial writing</p2>'''
 
+@app.route('/<path:x>')
+def info(x):
+    return f'{x}'
