ef148f387eb9fc1ad7ae85ec65a28f64a04d8bf4
timmashkov <bodomizer#yandex.ru>
2023-08-26 11:57:41 +0300

    added an html template

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'''<p1>Hello world</p1>
-<p2>This is the initial writing</p2>'''
+    return f'''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title> Home page </title>
+            <meta charset='utf-8'>
+        </head>
+        <body>
+            <p align='center'> The welcome data for user </p>
+        </body>
+    </html>
+    '''
 
 @app.route('/<path:x>')
 def info(x):
