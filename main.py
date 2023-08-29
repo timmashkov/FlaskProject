47e5671aef3f8d8f213ce18d5ff57b5fc346c73a
timmashkov <bodomizer#yandex.ru>
2023-08-29 13:17:23 +0300

    added second view function and second html file for it

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from flask import render_template
+from random import choice, shuffle, randrange, randint
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    words = ['Pretty', 'Beautiful', 'Gorgeous', 'Great']
+    shuffle(words)
+    return render_template('index.html', word=choice(words))
 
-@app.route('/<path:x>')
-def info(x):
-    return f'{x}'
+
+@app.route('/greeting')
+def greeting():
+    numbers = [i for i in range(randrange(randint(1, 1000)))]
+    string = f'Your lucky number today is {choice(numbers)}'
+    return render_template('greeting.html', phrase=string)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
